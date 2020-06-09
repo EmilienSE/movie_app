7a12877551ee46aa31e54f4a06e996581abc8bad
Emilien SECHILARIU <esechilariu@yahoo.fr>
2020-06-09 16:16:40 +0200

    components

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,7 +1,10 @@
 // Components/FilmDetail.js
 
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { View, Text, ActivityIndicator, ScrollView, Image } from 'react-native'
+import { getFilmDetailFromApi, getImageFromApi } from '../API/TMDBApi'
+import moment from 'moment'
+import numeral from 'numeral'
 
 class FilmDetail extends React.Component {
   constructor(props) {
@@ -12,17 +15,60 @@ class FilmDetail extends React.Component {
     }
   }
 
+  componentDidMount() {
+    getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
+      this.setState({
+        film: data,
+        isLoading: false
+      })
+    })
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+  }
+
+  _displayFilm() {
+    const { film } = this.state
+    if (film != undefined) {
+      return (
+        <ScrollView>
+          <Image
+            source={{uri: getImageFromApi(film.backdrop_path)}}
+          />
+          <Text>{film.title}</Text>
+          <Text>{film.overview}</Text>
+          <Text>Sorti le {moment(new Date(film.release_date)).format('DD/MM/YYYY')}</Text>
+          <Text>Note : {film.vote_average} / 10</Text>
+          <Text>Nombre de votes : {film.vote_count}</Text>
+          <Text>Budget : {numeral(film.budget).format('0,0[.]00 $')}</Text>
+          <Text>Genre(s) : {film.genres.map(function(genre){
+              return genre.name;
+            }).join(" / ")}
+          </Text>
+          <Text>Companie(s) : {film.production_companies.map(function(company){
+              return company.name;
+            }).join(" / ")}
+          </Text>
+        </ScrollView>
+      )
+    }
+  }
+
   render() {
     return (
-      <View style={styles.main_container} />
+      <View>
+        {this._displayLoading()}
+        {this._displayFilm()}
+      </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  main_container: {
-    flex: 1
-  }
-})
 
 export default FilmDetail
--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,21 +1,34 @@
 // Components/FilmItem.js
 
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { getImageFromApi } from '../API/TMDBApi'
 
 class FilmItem extends React.Component {
   render() {
+    const { film, displayDetailForFilm } = this.props
     return (
-      <View style={styles.date_container} />
+      <TouchableOpacity
+        onPress={() => displayDetailForFilm(film.id)}>
+        <Image
+          style={styles.image}
+          source={{uri: getImageFromApi(film.poster_path)}}
+        />
+        <View>
+          <View>
+            <Text>{film.title}</Text>
+            <Text>{film.vote_average}</Text>
+          </View>
+          <View>
+            <Text numberOfLines={6}>{film.overview}</Text>
+          </View>
+          <View>
+            <Text>Sorti le {film.release_date}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  main_container: {
-    height: 190,
-    flexDirection: 'row'
-  }
-})
 
 export default FilmItem
