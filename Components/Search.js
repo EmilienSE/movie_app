1b9d64e11e09dee248961d69bac574c03bc02ac3
argotax <maxoble.dl@gmail.com>
2020-06-12 13:53:25 +0200

    retour du touchableopacity

--- Components/Search.js
+++ Components/Search.js
@@ -65,13 +65,15 @@ class Search extends React.Component {
          onChangeText={(text) => this._searchTextInputChanged(text)}
          onSubmitEditing={() => this._searchFilms()}
        />
-       <Button title='Rechercher' onPress={() => this._searchFilms()}/>
+       <TouchableOpacity style={styles.button_search} onPress={() => this._searchFilms()}>
+         <Text style={styles.button_search_text}>Rechercher</Text>
+       </TouchableOpacity>
        <FilmList
-         films={this.state.films} // C'est bien le component Search qui récupère les films depuis l'API et on les transmet ici pour que le component FilmList les affiche
-         navigation={this.props.navigation} // Ici on transmet les informations de navigation pour permettre au component FilmList de naviguer vers le détail d'un film
-         loadFilms={this._loadFilms} // _loadFilm charge les films suivants, ça concerne l'API, le component FilmList va juste appeler cette méthode quand l'utilisateur aura parcouru tous les films et c'est le component Search qui lui fournira les films suivants
+         films={this.state.films}
+         navigation={this.props.navigation}
+         loadFilms={this._loadFilms}
          page={this.page}
-         totalPages={this.totalPages} // les infos page et totalPages vont être utile, côté component FilmList, pour ne pas déclencher l'évènement pour charger plus de film si on a atteint la dernière page
+         totalPages={this.totalPages}
        />
        {this._displayLoading()}
      </View>
